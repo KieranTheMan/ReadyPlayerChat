f04f592116c78cdb5dfe4cb15e1003de4b0edbbf
KieranFowler <kieran9fowler@gmail.com>
2025-02-13 11:52:05 +0000

    import ContactShadows included it in to Experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,21 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, ContactShadows } from "@react-three/drei";
 import Avatar from "./Avatar";
 
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
+      <OrbitControls
+        // minPolarAngle={Math.PI / 2.5}
+        // maxPolarAngle={Math.PI / 2.5}
+        // enablePan={false}
+        // enableZoom={false}
+        // rotateSpeed={0.1}
+      />
       <Avatar />
+      
       <Environment preset="sunset" />
+      <ContactShadows opacity={0.5} scale={10} blur={1} far={10} resolution={256} color="#000000" />
+
     </>
   );
 };
